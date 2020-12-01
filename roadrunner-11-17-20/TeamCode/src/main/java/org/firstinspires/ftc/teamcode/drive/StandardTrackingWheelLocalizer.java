390cceb0484fe866f0ff8ea8b45d8066f14c5f20
Katie SB <katescoles-bran23@marlborough.org>
2020-11-30 18:44:52 -0800

    changed lateral distance!

--- roadrunner-11-17-20/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ roadrunner-11-17-20/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,7 +31,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 1; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 13.6; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 13.347668457031272; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -2.12598; // in; offset of the lateral wheel
     
     public static double X_MULTIPLIER = 0.988685564504; // Multiplier in the X direction
